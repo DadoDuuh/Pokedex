0d8731a79345ef1c5a233c61fca235793fa41569
Eduardo Cesar Silva de Ávila <unkeniy8432@gmail.com>
2023-01-23 19:31:56 -0300

    Ajustes...(Cogitando alterar o driver de conexão)

--- api/src/infra/base/repository/connection.js
+++ api/src/infra/base/repository/connection.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
+
+
 
 
 mongoose.set('strictQuery', true)
 const connect = () => {
-  mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@devmonk.uput8o9.mongodb.net/DevMonk?retryWrites=true&w=majority`, {useNewUrlParser: true})
+  mongoose.connect(process.env.DB_CONSTR, {useNewUrlParser: true});
   const connection = mongoose.connection;
 
   connection.on("error", () => {
-    console.error("Erro ao conectar com o mongoDB!")
+    console.error("Erro ao conectar com o mongoDB!");
   })
   connection.on("open", () => {
-    console.log("MongoDB conectado com sucesso!")
+    console.log("MongoDB conectado com sucesso!");
   })
 }
 
